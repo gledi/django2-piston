99ee44ee72a228a069e7fee75a3a7aba66136788
Gledi Caushaj <411820+gledi@users.noreply.github.com>
2024-02-12 00:07:21 +0100

    fix: unicode_literals and raw string escape issue fix

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from future import standard_library
-standard_library.install_aliases()
 
 from setuptools import setup, find_packages
-
-import os
 
 setup(
     name = "django2-piston",
